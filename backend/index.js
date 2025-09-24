4337fc82fa064bf361c26144dfa44cbbc42b8272
JAIKANNA.B <117563781+jz6102@users.noreply.github.com>
2025-09-24 16:36:54 +0530

    task: Configure code for automatic deployment completed

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,29 @@ const express = require('express');
 const connectDB = require('./src/config/db');
 const cors = require('cors');
 require('dotenv').config();
+const allowedOrigins = [
+  'http://localhost:3000', 
+  process.env.CLIENT_URL    
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 const app = express();
 
-// Connect Database
 connectDB();
 
-// Middlewares
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ extended: false }));
 
-// Define Routes
 app.use('/api/auth', require('./src/routes/auth.routes'));
 app.use('/api/projects', require('./src/routes/project.routes'));
 app.use('/api/tasks', require('./src/routes/task.routes'));
@@ -21,4 +33,4 @@ const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-module.exports = app; // For testing
+module.exports = app; 
